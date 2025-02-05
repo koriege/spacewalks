9318ca7144065e68d89705e2c15eb003d2fed832
Konstantin Riege <koriege@gmail.com>
2025-02-05 15:18:42 +0100

    i/o as functions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,20 +1,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+"""
+data import and wrangling into a format able to be digested by pandas plot function
+"""
+def reads_json_to_df(input_file):
+    print(f'reading file {input_file}')
+    eva_df = pd.read_json(input_file, convert_dates=['date']) # loads in the raw data
+    eva_df['eva'] = eva_df['eva'].astype(float)
+    eva_df.dropna(axis=0, inplace=True) # removes NAs from data table
+    eva_df.sort_values('date', inplace=True)
+    return eva_df
+
+def write_df_to_csv(eva_df, output_file):
+    print(f'writing file {output_file}')
+    eva_df.to_csv(output_file, index=False)
+
 # Data source: https://data.nasa.gov/resource/eva.json (with modifications)
 input_file = open('./eva-data.json', 'r')
 output_file = open('./eva-data.csv', 'w')
 graph_file = './cumulative_eva_graph.png'
 
-"""
-data import and wrangling into a format able to be digested by pandas plot function
-"""
-eva_df = pd.read_json(input_file, convert_dates=['date']) # loads in the raw data
-eva_df['eva'] = eva_df['eva'].astype(float)
-eva_df.dropna(axis=0, inplace=True) # removes NAs from data table
-eva_df.sort_values('date', inplace=True)
-
-eva_df.to_csv(output_file, index=False) # 
+eva_df = reads_json_to_df(input_file)
+write_df_to_csv(eva_df, output_file)
 
 # split the time format HH:MM into duration as hours
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
